0fc98c0935afd141128f53b6cf9f3174b70e00de
Marek Nocoń <mnocon@users.noreply.github.com>
2021-05-19 18:32:36 +0200

    Verbose

--- bin/prepare_project_edition.sh
+++ bin/prepare_project_edition.sh
@@ -94,8 +94,8 @@ if [ -f ./${DEPENDENCY_PACKAGE_NAME}/dependencies.json ]; then
         PACKAGE_NAME=$(cat dependencies.json | jq -r .[$i].package)
         REQUIREMENT=$(cat dependencies.json | jq -r .[$i].requirement)
         # docker exec install_dependencies composer config repositories.$(uuidgen) vcs "$REPO_URL"
-        docker exec install_dependencies composer require ${PACKAGE_NAME}:"$REQUIREMENT" --no-scripts
-        docker exec install_dependencies composer sync-recipes ${PACKAGE_NAME} --force
+        docker exec install_dependencies composer require ${PACKAGE_NAME}:"$REQUIREMENT" --no-scripts -v
+        docker exec install_dependencies composer sync-recipes ${PACKAGE_NAME} --force -v
     done
 fi
 
