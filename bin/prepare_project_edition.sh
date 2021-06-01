2811f02ea3ba3301204720201b3e21b75b4555e7
Marek Nocoń <mnocon@users.noreply.github.com>
2021-06-01 10:57:51 +0200

    Downgrade PHPUnit before installing BehatBundle

--- bin/prepare_project_edition.sh
+++ bin/prepare_project_edition.sh
@@ -70,7 +70,9 @@ composer config repositories.localDependency "$JSON_STRING"
 docker exec install_dependencies composer update
 docker exec -e APP_ENV=dev install_dependencies composer require ibexa/${PROJECT_EDITION}:${PROJECT_VERSION} -W
 
-docker exec install_dependencies composer require --dev ezsystems/behatbundle:^8.3.x-dev --no-scripts
+# TMP - install phpunit ^8.0 first
+docker exec install_dependencies composer require --dev phpunit/phpunit:^8.0 -W --no-scripts
+docker exec install_dependencies composer require --dev ezsystems/behatbundle:^8.3.x-dev -W --no-scripts
 docker exec install_dependencies composer sync-recipes ezsystems/behatbundle --force
 
 # Init a repository to avoid Composer asking questions
