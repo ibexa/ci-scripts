041d46dead86d433096f244b2e3be13b2fc2ebe2
Marek Nocoń <mnocon@users.noreply.github.com>
2021-06-30 11:06:50 +0200

    [TMP] Installed kernel

--- bin/prepare_project_edition.sh
+++ bin/prepare_project_edition.sh
@@ -68,7 +68,8 @@ composer config repositories.localDependency "$JSON_STRING"
 
 # Install correct product variant
 docker exec install_dependencies composer update
-docker exec -e APP_ENV=dev install_dependencies composer require ibexa/oss:"dev-symfony53 as 3.3.x-dev" -W
+docker exec -e APP_ENV=dev install_dependencies composer require ezsystems/ezplatform-kernel:"dev-symfony53 as 3.3.x-dev" --no-update
+docker exec -e APP_ENV=dev install_dependencies composer require ibexa/oss:"dev-symfony53 as 3.3.x-dev" --no-update
 docker exec -e APP_ENV=dev install_dependencies composer require ibexa/${PROJECT_EDITION}:${PROJECT_VERSION} -W
 
 # TMP - install phpunit ^8.0 first
