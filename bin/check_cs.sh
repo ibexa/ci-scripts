4132a914bfdbd5ef81e1fff28a77cdeebac73b1e
Marek Nocoń <mnocon@users.noreply.github.com>
2022-01-05 11:05:22 +0100

    GitHub actions refactor (#31)
    * Removed unused scripts
    
    * Adjusted regression command to Github Actions setup
    
    * Added scripts setting up project for all versions
    
    * Use SSH by default

--- bin/check_cs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-./vendor/bin/php-cs-fixer fix -v --dry-run --show-progress=estimating
--- src/Command/RunRegressionCommand.php
+++ src/Command/RunRegressionCommand.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace Ibexa\Platform\ContiniousIntegrationScripts\Command;
 
 use Cz\Git\GitException;
+use JsonException;
 use Cz\Git\GitRepository;
 use Github\Client;
 use Ibexa\Platform\ContiniousIntegrationScripts\Helper\ComposerHelper;
@@ -21,11 +22,11 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class RunRegressionCommand extends Command
 {
-    private const REPO_OWNER = 'ezsystems';
-
-    private const REPO_NAME = 'ezplatform-page-builder';
+    private const REPO_OWNER = 'ibexa';
 
     private const COMMIT_MESSAGE = '[TMP] Run regression';
+
+    private const PRODUCT_EDITIONS = ['oss', 'content', 'experience', 'commerce'];
 
     /** @var ?string */
     private $token;
@@ -43,7 +44,7 @@ class RunRegressionCommand extends Command
                 if (preg_match('/^(\d+)\.(\d+)$/', $answer) === 0) {
                     throw new \RuntimeException(
                         sprintf(
-                            'Unrecognised version format: %s. Please use format X.Y instead, e.g. 3.3',
+                            'Unrecognised version format: %s. Please use format X.Y instead, e.g. 3.3, 4.0',
                         $answer)
                     );
                 }
@@ -53,6 +54,24 @@ class RunRegressionCommand extends Command
 
             $input->setArgument('productVersion', $productVersion);
         }
+
+        if (!$input->getArgument('productEdition')) {
+            $productEdition = $io->ask('Please enter the Ibexa DXP edition', 'oss', static function (string $answer): string {
+                if (!in_array($answer, self::PRODUCT_EDITIONS)) {
+                    throw new \RuntimeException(
+                        sprintf(
+                            'Unrecognised edition: %s. Please choose one of: %s',
+                        $answer,
+                        implode(',', self::PRODUCT_EDITIONS)
+                        )
+                    );
+                }
+
+                return $answer;
+            });
+
+            $input->setArgument('productEdition', $productEdition);
+        }
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
@@ -60,47 +79,48 @@ class RunRegressionCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $this->token = $input->getArgument('token');
         $productVersion = $input->getArgument('productVersion');
+        $productEdition = $input->getArgument('productEdition');
 
         $this->validate();
 
-        $pageBuilderBaseBranch = $this->mapProductVersionToPageBuilder($productVersion);
+        $baseBranch = $this->getBaseBranch($productVersion);
         $regressionBranchName = uniqid('tmp_regression_', true);
 
         try {
             $repo = GitRepository::cloneRepository(
-                sprintf('https://github.com/%s/%s.git', self::REPO_OWNER, self::REPO_NAME),
-                null, ['-b' => $pageBuilderBaseBranch]
+                sprintf('git@github.com:%s/%s.git', self::REPO_OWNER, $productEdition),
+                null, ['-b' => $baseBranch]
             );
         } catch (GitException $exception) {
-            // fallback to SSH if HTTPS fails
+            // fallback to HTTPS if SSH fails
             $repo = GitRepository::cloneRepository(
-                sprintf('git@github.com:%s/%s.git', self::REPO_OWNER, self::REPO_NAME),
-                null, ['-b' => $pageBuilderBaseBranch]
+                sprintf('https://github.com/%s/%s.git', self::REPO_OWNER, $productEdition),
+                null, ['-b' => $baseBranch]
             );
         }
 
         $repo->createBranch($regressionBranchName, true);
-        $repo->removeBranch($pageBuilderBaseBranch);
+        $repo->removeBranch($baseBranch);
 
-        copy(LinkDependenciesCommand::DEPENDENCIES_FILE, self::REPO_NAME . \DIRECTORY_SEPARATOR . LinkDependenciesCommand::DEPENDENCIES_FILE);
+        copy(LinkDependenciesCommand::DEPENDENCIES_FILE, $productEdition . \DIRECTORY_SEPARATOR . LinkDependenciesCommand::DEPENDENCIES_FILE);
 
-        $repo->addFile('dependencies.json');
+        $repo->addFile(LinkDependenciesCommand::DEPENDENCIES_FILE);
         $repo->commit(self::COMMIT_MESSAGE);
         $repo->push('origin', [$regressionBranchName, '-u']);
 
-        $io->success(sprintf('Successfuly pushed to %s/%s a branch called: %s', self::REPO_OWNER, self::REPO_NAME, $regressionBranchName));
+        $io->success(sprintf('Successfuly pushed to %s/%s a branch called: %s', self::REPO_OWNER, $productEdition, $regressionBranchName));
 
         $client = new Client();
         if ($this->token) {
             $client->authenticate($this->token, null, Client::AUTH_ACCESS_TOKEN);
         }
 
-        $this->waitUntilBranchExists($client, $regressionBranchName);
+        $this->waitUntilBranchExists($client, $productEdition, $regressionBranchName);
 
-        $response = $client->pullRequests()->create(self::REPO_OWNER, self::REPO_NAME,
+        $response = $client->pullRequests()->create(self::REPO_OWNER, $productEdition,
         [
             'title' => 'Run regression for IBX-XXXX',
-            'base' => $pageBuilderBaseBranch,
+            'base' => $baseBranch,
             'head' => $regressionBranchName,
             'body' => 'Please add your description here.',
             'draft' => 'true',
@@ -108,9 +128,9 @@ class RunRegressionCommand extends Command
 
         $io->success(sprintf('Created PR, please see: %s', $response['_links']['html']['href']));
 
-        if ($io->confirm(sprintf('Do you want to remove the created %s directory?', self::REPO_NAME), true)) {
+        if ($io->confirm(sprintf('Do you want to remove the created %s directory?', $productEdition), true)) {
             $fs = new Filesystem();
-            $fs->remove(self::REPO_NAME);
+            $fs->remove($productEdition);
         }
 
         return Command::SUCCESS;
@@ -122,6 +142,7 @@ class RunRegressionCommand extends Command
             ->setName('regression:run')
             ->setDescription('Triggers a regression run on Travis')
             ->addArgument('productVersion', InputArgument::REQUIRED, 'Ibexa DXP version')
+            ->addArgument('productEdition', InputArgument::REQUIRED, 'Ibexa DXP edition')
             ->addArgument('token', InputArgument::OPTIONAL, 'GitHub token')
         ;
     }
@@ -141,46 +162,24 @@ class RunRegressionCommand extends Command
             );
         }
 
-        $dependencies = json_decode($dependenciesFile, true, 512, JSON_THROW_ON_ERROR);
-
-        foreach ($dependencies as $dependency) {
-            if ($dependency['package'] === 'ezsystems/ezplatform-page-builder') {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Page Builder dependency detected. Simply attach the %s file to that PR and use the commit messgae "%s"',
-                        LinkDependenciesCommand::DEPENDENCIES_FILE,
-                        self::COMMIT_MESSAGE
-                    )
-                );
-            }
+        try {
+            json_decode($dependenciesFile, true, 512, JSON_THROW_ON_ERROR);
+        }
+        catch (JsonException $e)
+        {
+            throw new \RuntimeException(
+                sprintf("File '%s' is not a valid JSON.", LinkDependenciesCommand::DEPENDENCIES_FILE)
+            );
         }
     }
 
-    private function mapProductVersionToPageBuilder(string $productVersion): string
-    {
-        switch ($productVersion) {
-            case '3.3':
-                return '2.3';
-            case '4.0':
-                return 'master';
-            case '2.5':
-            case '3.2':
-            default:
-                throw new \RuntimeException(
-                    sprintf(
-                        'Unsupported version %s. Please contact QA team',
-                        $productVersion
-                    ));
-        }
-    }
-
-    private function waitUntilBranchExists(Client $client, string $branchNme): void
+    private function waitUntilBranchExists(Client $client, string $productEdition, string $branchName): void
     {
         $counter = 0;
         $success = false;
         while ($counter < 5) {
             try {
-                $client->repo()->branches(self::REPO_OWNER, self::REPO_NAME, $branchNme);
+                $client->repo()->branches(self::REPO_OWNER, $productEdition, $branchName);
                 $success = true;
                 break;
             } catch (\RuntimeException $e) {
@@ -192,5 +191,10 @@ class RunRegressionCommand extends Command
         if (!$success) {
             throw new \RuntimeException('Pushed branch not found using GitHub API.');
         }
+    }
+
+    private function getBaseBranch(string $productVersion): string
+    {
+        return $productVersion === "4.0" ? "master" : $productVersion;
     }
 }
