abe1f37e617d9e1599f8ce3b002ddf98b0d04122
Marek Nocoń <mnocon@users.noreply.github.com>
2022-02-15 12:51:48 +0100

    Revert "[Workaround] Set framework.assets.json_manifest_path to null"
    This reverts commit e807edbbccb993052f407f5e9eb0e752ae64b1f6.

--- bin/^4.0.x-dev/prepare_project_edition.sh
+++ bin/^4.0.x-dev/prepare_project_edition.sh
@@ -115,9 +115,6 @@ fi
 # Create a default Behat configuration file
 cp "behat_ibexa_${PROJECT_EDITION}.yaml" behat.yaml
 
-# TMP Workaround for assets issue
-sudo sed -i "s/'%kernel.project_dir%\/public\/build\/manifest.json'/~/g" config/packages/webpack_encore.yaml
-
 # Depenencies are installed and container can be removed
 docker container stop install_dependencies
 docker container rm install_dependencies
@@ -131,7 +128,6 @@ docker-compose --env-file=.env exec -T app sh -c 'chown -R www-data:www-data /va
 
 # Rebuild container
 docker-compose --env-file=.env exec -T --user www-data app sh -c "rm -rf var/cache/*"
-
 echo '> Clear cache & generate assets'
 docker-compose --env-file=.env exec -T --user www-data app sh -c "composer run post-install-cmd"
 
