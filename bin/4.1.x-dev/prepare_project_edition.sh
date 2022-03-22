0ec84c5284e0e7036001d269d044cae8d2bcf278
Marek Nocoń <mnocon@users.noreply.github.com>
2022-03-22 16:15:28 +0100

    Fixed website-skeleton versions

--- bin/4.1.x-dev/prepare_project_edition.sh
+++ bin/4.1.x-dev/prepare_project_edition.sh
@@ -8,7 +8,7 @@ export PHP_IMAGE=${4-ezsystems/php:7.4-v2-node16}
 
 echo "> Setting up website skeleton"
 PROJECT_BUILD_DIR=${HOME}/build/project
-composer create-project ibexa/website-skeleton:^4.0@dev ${PROJECT_BUILD_DIR} --no-install --no-scripts 
+composer create-project ibexa/website-skeleton:~4.1.0@dev ${PROJECT_BUILD_DIR} --no-install --no-scripts 
 
 if [[ -n "${DOCKER_PASSWORD}" ]]; then
     echo "> Set up Docker credentials"
