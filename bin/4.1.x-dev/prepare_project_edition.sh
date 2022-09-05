4385d7539f26a99a44563f8079b3898c96bc53ab
Marek Nocoń <mnocon@users.noreply.github.com>
2022-09-05 13:00:18 +0200

    Switched to new ibexa/docker versioning schema

--- bin/4.1.x-dev/prepare_project_edition.sh
+++ bin/4.1.x-dev/prepare_project_edition.sh
@@ -83,7 +83,7 @@ JSON_STRING=$( jq -n \
 composer config repositories.localDependency "$JSON_STRING"
 
 # Install Behat and Docker packages
-docker exec install_dependencies composer require ibexa/behat:~4.1.x-dev ibexa/docker:^0.2.x-dev --no-scripts --no-update
+docker exec install_dependencies composer require ibexa/behat:$PROJECT_VERSION ibexa/docker:$PROJECT_VERSION --no-scripts --no-update
 
 # Add other dependencies if required
 if [ -f dependencies.json ]; then
