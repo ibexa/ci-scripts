344379b430971381e73e1b0577f5a3fb8645f9c0
Michał Szołtysek <michal.szoltysek@ez.no>
2024-03-08 08:46:15 +0100

    Revert "reindex refactor"
    This reverts commit a74ad17cf28585bf0aa80266ac637e8d5f82a888.

--- bin/4.6.x-dev/prepare_project_edition.sh
+++ bin/4.6.x-dev/prepare_project_edition.sh
@@ -173,7 +173,8 @@ echo '> Install data'
 if [[ "$COMPOSE_FILE" == *"elastic.yml"* ]]; then
     docker-compose --env-file=.env exec -T --user www-data app sh -c "php bin/console ibexa:elasticsearch:put-index-template"
 fi
-docker-compose --env-file=.env exec -T --user www-data app sh -c "php /scripts/wait_for_db.php; php bin/console ibexa:install"
+docker-compose --env-file=.env exec -T --user www-data app sh -c "php /scripts/wait_for_db.php; php bin/console ibexa:install --skip-indexing"
+docker-compose --env-file=.env exec -T --user www-data app sh -c "php bin/console ibexa:reindex"
 
 echo '> Generate GraphQL schema'
 docker-compose --env-file=.env exec -T --user www-data app sh -c "php bin/console ibexa:graphql:generate-schema"
