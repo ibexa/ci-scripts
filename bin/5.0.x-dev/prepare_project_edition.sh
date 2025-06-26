22a2323c53349ce92fe82aed6fa02f5c9a569ebd
tomaszszopinski <tomasz.szopinski@ez.no>
2025-06-26 09:58:34 +0200

    another approach to permissions

--- bin/5.0.x-dev/prepare_project_edition.sh
+++ bin/5.0.x-dev/prepare_project_edition.sh
@@ -271,8 +271,8 @@ docker compose --env-file=.env exec -T --user www-data app sh -c "composer requi
 docker compose --env-file=.env exec -T --user www-data app sh -c "php bin/console ibexa:graphql:generate-schema"
 
 docker compose --env-file=.env exec -T --user www-data app sh -c "composer recipes:install ibexa/behat --force --reset"
-chmod +r "behat_ibexa_${PROJECT_EDITION}.yaml"
-cp "behat_ibexa_${PROJECT_EDITION}.yaml" behat.yaml
+
+docker compose --env-file=.env exec -T --user www-data app sh -c "cp "behat_ibexa_commerce.yaml" behat.yaml"
 
 
 echo '> Done, ready to run tests'
