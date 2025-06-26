742c12aaf03b48825080503531dab83cc88b3038
tomaszszopinski <tomasz.szopinski@ez.no>
2025-06-26 11:14:18 +0200

    more yaml removal

--- bin/5.0.x-dev/prepare_project_edition.sh
+++ bin/5.0.x-dev/prepare_project_edition.sh
@@ -270,6 +270,7 @@ docker compose --env-file=.env exec -T --user www-data app sh -c "php bin/consol
 docker compose --env-file=.env exec -T --user www-data app sh -c "composer require ibexa/graphql --no-interaction"
 docker compose --env-file=.env exec -T --user www-data app sh -c "php bin/console ibexa:graphql:generate-schema"
 
+docker compose --env-file=.env exec -T --user www-data app sh -c "rm behat_ibexa_commerce.yaml"
 docker compose --env-file=.env exec -T --user www-data app sh -c "composer recipes:install ibexa/behat --force --reset"
 
 docker compose --env-file=.env exec -T --user www-data app sh -c "cp behat_ibexa_commerce.yaml behat.yaml"
