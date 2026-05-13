3c2deebab216401397f28e087a24f56d5ad701bd
katarzynazawada <42930265+katarzynazawada@users.noreply.github.com>
2026-05-13 11:55:52 +0200

    IBX-11657: Added connector-quable to CI (#135)
    * IBX-11657: Added connector-quable to CI
    
    * IBX-11657: Adedd connector-quable for stable
    
    * IBX-11657: Added connector-quable to v6
    
    * IBX-11657: Changed stable parameters

--- bin/5.0.x-dev/prepare_project_edition.sh
+++ bin/5.0.x-dev/prepare_project_edition.sh
@@ -119,6 +119,11 @@ if [[ "$PROJECT_EDITION" != "oss" ]]; then
   docker exec install_dependencies composer require ibexa/connector-gemini:$PROJECT_VERSION --no-scripts --ansi --no-update
   docker exec install_dependencies composer require ibexa/integrated-help:$PROJECT_VERSION --no-scripts --ansi --no-update
   docker exec install_dependencies composer require ibexa/connector-raptor:$PROJECT_VERSION --no-scripts --ansi --no-update
+
+  if [[ "${INSTALL_CONNECTOR_QUABLE:-false}" == "true" ]]; then
+    docker exec install_dependencies composer require ibexa/connector-quable:$PROJECT_VERSION --no-scripts --ansi --no-update
+  fi
+  
 fi
 if [[ "$PROJECT_EDITION" == "commerce" ]]; then
   docker exec install_dependencies composer require ibexa/shopping-list:$PROJECT_VERSION --no-scripts --ansi --no-update
