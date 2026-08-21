c95f393c259248eee1dd276192e5b30099d9d609
Andrew Longosz <alongosz@users.noreply.github.com>
2026-08-21 11:15:31 +0200

    [Prepare Project Edition] Moved composer dump before composer update

--- bin/4.6.x-dev/prepare_project_edition.sh
+++ bin/4.6.x-dev/prepare_project_edition.sh
@@ -159,8 +159,6 @@ if [ -f dependencies.json ]; then
     done
 fi
 
-docker exec install_dependencies composer update --no-scripts
-
 # Enable FriendsOfBehat SymfonyExtension in the Behat env
 sudo sed -i "s/\['test' => true\]/\['test' => true, 'behat' => true\]/g" config/bundles.php
 
@@ -172,6 +170,9 @@ fi
 
 echo "> Display composer.json for debugging"
 cat composer.json
+
+echo "> Performing composer update --no-scripts"
+docker exec install_dependencies composer update --no-scripts
 
 # Create a default Behat configuration file
 cp "behat_ibexa_${PROJECT_EDITION}.yaml" behat.yaml
