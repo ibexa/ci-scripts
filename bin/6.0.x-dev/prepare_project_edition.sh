db673cbca320ff43d12de124284064f0184ec1b5
Dawid Parafinski <dawid.parafinski@ibexa.co>
2026-09-07 12:36:26 +0200

    Relied on the ibexabehat default configuration instead of removing recipe files

--- bin/6.0.x-dev/prepare_project_edition.sh
+++ bin/6.0.x-dev/prepare_project_edition.sh
@@ -160,9 +160,6 @@ echo "> Display composer.json for debugging"
 cat composer.json
 
 # Create a default Behat configuration file
-# Remove Behat configuration files added by recipes (e.g. behat.yml.dist from friends-of-behat/symfony-extension),
-# they take precedence over behat.php in Behat's configuration discovery
-rm -f behat.yaml behat.yml behat.yaml.dist behat.yml.dist behat.dist.yaml behat.dist.yml behat.php behat.dist.php
 if [ -f "behat_ibexa_${PROJECT_EDITION}.php" ]; then
     cp "behat_ibexa_${PROJECT_EDITION}.php" behat.php
 else
